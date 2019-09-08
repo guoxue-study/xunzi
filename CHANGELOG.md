2a3b5e62556bbf366e205ab274fe1925691c3332
Tyr Chen <tyr.chen@gmail.com>
2019-09-08 09:56:07 -0700

    fix typo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.2.1 (September 08, 2019)
+  - fix typo
+
 ## 0.2.0 (September 08, 2019)
   - add all audios
 
--- tools/cli/gen_index.js
+++ tools/cli/gen_index.js
@@ -5,13 +5,13 @@ const md = require('markdown-it')();
 const R = require('ramda');
 
 function gen_html(body) {
-	return `
+  return `
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>孟子</title>
+    <title>荀子</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css" />
     <style>
       * {font-size: 20px;}
@@ -41,49 +41,49 @@ function gen_html(body) {
 }
 
 async function read(filename) {
-	const content = JSON.parse(await fs.readFile(filename, 'utf8'));
-	const name = path.basename(filename, '.json');
-	const topic = content[0].text.replace('# ', '');
-	return `* [${topic}](${name}.html)`;
+  const content = JSON.parse(await fs.readFile(filename, 'utf8'));
+  const name = path.basename(filename, '.json');
+  const topic = content[0].text.replace('# ', '');
+  return `* [${topic}](${name}.html)`;
 }
 
 async function processFile(templateFile, outFile, content) {
-	try {
-		let data = await fs.readFile(templateFile, 'utf8');
-		let result = md.render(`${data.split('---')[0]}---\n\n${content}`);
-		await fs.writeFile(outFile, gen_html(result));
-	} catch (e) {
-		console.log(e);
-	}
+  try {
+    let data = await fs.readFile(templateFile, 'utf8');
+    let result = md.render(`${data.split('---')[0]}---\n\n${content}`);
+    await fs.writeFile(outFile, gen_html(result));
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function main() {
-	const argv = require('yargs')
-		.option('input', {
-			alias: 'i',
-			describe: 'input path containing all json files'
-		})
-		.option('template', {
-			alias: 't',
-			describe: 'template filename'
-		})
-		.option('output', {
-			alias: 'o',
-			describe: 'output path to put generated md files'
-		})
-		.demandOption(['input', 'template', 'output'], 'Please provide input path, template file and output file')
-		.help().argv;
+  const argv = require('yargs')
+    .option('input', {
+      alias: 'i',
+      describe: 'input path containing all json files'
+    })
+    .option('template', {
+      alias: 't',
+      describe: 'template filename'
+    })
+    .option('output', {
+      alias: 'o',
+      describe: 'output path to put generated md files'
+    })
+    .demandOption(['input', 'template', 'output'], 'Please provide input path, template file and output file')
+    .help().argv;
 
-	const results = Array(32)
-		.fill()
-		.map((x, i) => {
-			const filename = path.join(argv.i, `${(i + 1).toString().padStart(2, '0')}.json`);
-			return read(filename);
-		});
+  const results = Array(32)
+    .fill()
+    .map((x, i) => {
+      const filename = path.join(argv.i, `${(i + 1).toString().padStart(2, '0')}.json`);
+      return read(filename);
+    });
 
-	const content = await Promise.all(results);
+  const content = await Promise.all(results);
 
-	await processFile(argv.t, argv.o, content.join('\n'));
+  await processFile(argv.t, argv.o, content.join('\n'));
 }
 
 main();
